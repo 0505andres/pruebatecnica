319a44ba965b675e41e2fd74d5e82670cc1ee545
0505andres <77692563+0505andres@users.noreply.github.com>
2022-10-20 13:12:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/prueba-tecnica'));
+app.use(express.static(__dirname + '/dist/PruebaTecnica'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/prueba-tecnica/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/PruebaTecnica/index.html'));
 });
 app.listen(process.env.PORT || 8181);
